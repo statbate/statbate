d6229fb314707b7cd5dcd5765f3a9a270141d3cd
poiuty <poiuty@lepus.su>
2019-12-20 17:30:21 +0300

    update cli

--- cli/import.php
+++ cli/import.php
@@ -3,19 +3,35 @@ if(php_sapi_name() != "cli"){
 	die;
 }
 require_once('/var/www/chaturbate100.com/func.php');
-$query = $db->query("select min(id), max(id) from `stat`");
-$x = $query->fetch();
-for($i = $x['0']; $i < $x['1']; $i++){
-	$query = $db->query("SELECT * FROM `stat` WHERE `id` = $i");
+
+$query = $db->query("select min(id) as min, max(id) as max from `stat`");
+if($query->rowCount() == 0){
+	die("no data\n");
+}
+$row = $query->fetch();
+
+$min = $row['min'];
+$max = $row['max'];
+
+$query = $sphinx->query("SELECT max(id) as max FROM stat");
+if($query->rowCount() != 0){
+	$min = $query->fetch()['max']+1;
+}
+
+$step = 2048;
+
+for($i = $min; $i < $max; $i+=$step){
+	$sql = '';
+	$start = $i;
+	$end = $start+$step;
+	$query = $db->query("SELECT * FROM `stat` WHERE `id` >= $start AND `id` < $end");
 	if($query->rowCount() == 0){
 		continue;
 	}
-	$row = $query->fetch();
-	$query = $sphinx->prepare("INSERT INTO stat VALUES (:id, '', :did, :rid, :token, :time)");
-	$query->bindParam(':id', $row['id']);
-	$query->bindParam(':did', $row['did']);
-	$query->bindParam(':rid', $row['rid']);
-	$query->bindParam(':token', $row['token']);
-	$query->bindParam(':time', $row['time']);
-	$query->execute();
+	while($row = $query->fetch()){
+		$sql .= "({$row['id']}, '', {$row['did']}, {$row['rid']}, {$row['token']}, {$row['time']}),";
+	}
+	$sql = rtrim($sql, ',');
+	$insert = $sphinx->prepare("INSERT INTO stat VALUES $sql");	
+	$insert->execute();
 }
