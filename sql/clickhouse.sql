dca4bf7831b5809caac78acf49ab37333a821ac5
poiuty <poiuty@lepus.su>
2021-12-18 18:54:33 +0300

    update sql

---
+++ sql/clickhouse.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE statbate;
+USE statbate;
+
+CREATE TABLE room
+(
+    id Int32,
+    gender Int32
+) ENGINE = MySQL('127.0.0.1:3306', 'base', 'room', 'user', 'passwd');
+
+CREATE TABLE stat
+(
+    id    Int32,
+    did   Int32,
+    rid   Int32,
+    token Int32,
+    time  Int64
+) ENGINE = MergeTree ORDER BY rid
+SETTINGS index_granularity = 8192;
