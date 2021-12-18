3296533ae1e143f06fef14ca1c64bc77c25082e9
poiuty <poiuty@lepus.su>
2021-12-18 18:46:48 +0300

    update sql

--- sql/stat.sql
+++ sql/stat.sql
@@ -2,6 +2,7 @@ CREATE TABLE `cache` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
+  `info` varbinary(1024) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `date` (`date`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=binary;
@@ -13,20 +14,11 @@ CREATE TABLE `donator` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=binary ROW_FORMAT=DYNAMIC;
 
-CREATE TABLE `online` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rid` int(11) NOT NULL,
-  `online` smallint(6) NOT NULL,
-  `time` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `rid` (`rid`,`time`),
-  KEY `time` (`time`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=binary ROW_FORMAT=DYNAMIC;
-
 CREATE TABLE `room` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varbinary(30) NOT NULL,
   `gender` tinyint(1) NOT NULL DEFAULT 0,
+  `fans` int(11) NOT NULL DEFAULT 0,
   `last` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
@@ -40,5 +32,6 @@ CREATE TABLE `stat` (
   `token` int(11) NOT NULL,
   `time` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `rid` (`rid`,`time`)
+  KEY `rid` (`rid`),
+  KEY `time` (`time`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=binary ROW_FORMAT=DYNAMIC;
