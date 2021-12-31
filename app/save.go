68d208e9e517edfed09d5387230a97a367e311ef
poiuty <poiuty@lepus.su>
2021-12-31 21:12:20 +0300

    Update save.go

--- app/save.go
+++ app/save.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"time"
-)
-
-type tableID struct {
+type tID struct {
 	Id     int64   `db:"id"`
 }
 
@@ -18,33 +14,25 @@ type Save struct {
 	donate chan *saveData
 }
 
-func saveDonate(did, rid, token int64) int64 {
-	res, _ := Mysql.Exec("INSERT INTO `stat` (`did`, `rid`, `token`, `time`) VALUES (?, ?, ?, unix_timestamp(now()))", did, rid, token)
-	id, _ := res.LastInsertId()
-	return id
-}
-
-func saveClickhouse(id, did, rid, token int64) {
-	tx, _ := Clickhouse.Begin()
-	tx.Exec("INSERT INTO stat VALUES (?, ?, ?, ?, ?)", id, did, rid, token, time.Now().Unix())
-	tx.Commit()
-}
-
-func getDonatorID(name string) int64 {
-	var donator tableID
-	err := Mysql.Get(&donator, "SELECT id FROM donator WHERE name=?", name)
+func saveDonate(name string, rid, token int64) {
+	donator := new(tID)
+	err := Mysql.Get(donator, "SELECT id FROM donator WHERE name=?", name)
 	if err != nil {		
 		res, _ := Mysql.Exec("INSERT INTO donator (`name`) VALUES (?)", name)
-		id, _ := res.LastInsertId()
-		return id
+		donator.Id, _ = res.LastInsertId()
 	}
-	return donator.Id
+	res, _ := Mysql.Exec("INSERT INTO `stat` (`did`, `rid`, `token`, `time`) VALUES (?, ?, ?, unix_timestamp(now()))", donator.Id, rid, token)
+	id, err := res.LastInsertId(); if err == nil {
+		tx, _ := Clickhouse.Begin()
+		tx.Exec("INSERT INTO stat VALUES (?, ?, ?, ?, toUInt64(now()))", id, donator.Id, rid, token)
+		tx.Commit()
+	}
 }
 
-func getRoomInfo(name string) (tableID, bool) {
+func getRoomInfo(name string) (*tID, bool) {
 	result := true
-	var room tableID
-	err := Mysql.Get(&room, "SELECT id FROM room WHERE name=?", name)
+	room := new(tID)
+	err := Mysql.Get(room, "SELECT id FROM room WHERE name=?", name)
 	if err != nil {
 		result = false
 	}
@@ -55,9 +43,7 @@ func saveBase(s *Save){
 	for {
 		select {
 			case info := <-s.donate:
-			d := getDonatorID(info.donator)
-			lastID := saveDonate(d, info.rid, info.token)
-			saveClickhouse(lastID, d, info.rid, info.token)
+			saveDonate(info.donator, info.rid, info.token)
 		}
 	}
 }
