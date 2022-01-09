20221cc876a2ae406f097067d3b3b4c4678f47c9
poiuty <poiuty@lepus.su>
2022-01-09 17:12:50 +0300

    update db

--- sql/clickhouse.sql
+++ sql/clickhouse.sql
@@ -3,16 +3,22 @@ USE statbate;
 
 CREATE TABLE room
 (
-    id Int32,
-    gender Int32
+	id Int32,
+	gender UInt8
 ) ENGINE = MySQL('127.0.0.1:3306', 'base', 'room', 'user', 'passwd');
+
 
 CREATE TABLE stat
 (
-    id    Int32,
-    did   Int32,
-    rid   Int32,
-    token Int32,
-    time  Int32
-) ENGINE = MergeTree ORDER BY rid
+	did   UInt32,
+	rid   UInt32,
+	token UInt32,
+	time  Date,
+	INDEX a did TYPE set(0) GRANULARITY 1,
+	INDEX b rid TYPE set(0) GRANULARITY 1
+) 
+ENGINE = MergeTree()
+PARTITION BY toYYYYMMDD(time)
+ORDER BY (time, rid, did)
+PRIMARY KEY (time)
 SETTINGS index_granularity = 8192;
